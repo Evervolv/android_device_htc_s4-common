aeb8fcca5f427621a8b3bad415d330a2e3b9cd77
Ethan Chen <intervigil@gmail.com>
2013-08-04 12:26:16 -0700

    s4-common: set OpenGL ES version to 2.0
    * According to Qualcomm, Adreno 225 supports up to OpenGL ES 2.0 only
    
    Change-Id: I022d956659db1bffaac47740d9ff13d62b8aff3a

--- s4.mk
+++ s4.mk
@@ -71,6 +71,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qmienabled=true \
     ro.baseband.arch=msm \
+    ro.opengles.version=131072 \
     ro.product.wireless=WCN3660 \
     ro.qualcomm.bt.hci_transport=smd
 
